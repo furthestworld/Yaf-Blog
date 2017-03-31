6c5ff22fe54bd37ba3de994af40d09c5644d2e7a
xuan <zhangpengxuan@yundun.com>
2017-03-31 16:34:13 +0800

    更新小问题

--- index.php
+++ index.php
@@ -1,11 +1,12 @@
 <?php
-
+// 除了 E_NOTICE，报告其他所有错误
+error_reporting(E_ALL ^ E_NOTICE);
 define('APPLICATION_PATH', dirname(__FILE__));
-define('BASE_URL',"http://www.programcat.com/");
+define('BASE_URL', "http://blog.yaf.vm/");
+define('STATIC_PUBLIC', BASE_URL  . '/');
 define('CACHE_TIME', 3600);
-define('IP',"123.57.52.70"); 
+define('IP', "127.0.0.1");
 
-$application = new Yaf_Application( APPLICATION_PATH . "/conf/application.ini");
+$application = new Yaf_Application(APPLICATION_PATH . "/conf/application.ini");
 
 $application->bootstrap()->run();
-?>
